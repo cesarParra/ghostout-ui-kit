27abf52367cc9ff6117785c96aef4fe21e989805
Cesar Parra <cesar.parra@gmail.com>
2018-09-21 16:54:12 -0400

    Alert component tests.

--- src/components/Alert/Alert.test.js
+++ src/components/Alert/Alert.test.js
@@ -6,6 +6,31 @@ import Alert from './Alert';
 
 describe('<Alert />', () => {
   it('renders children when passed in', () => {
-    expect(true).to.equal(true);
+    const alertComponent = shallow((
+    <Alert>
+        <div className="test" />
+    </Alert>));
+
+    expect(alertComponent.contains(<div className="test" />)).to.equal(true);
+  });
+
+  it('has the notification class name', () => {
+    const alertComponent = shallow((
+    <Alert type="danger">
+      Danger
+    </Alert>));
+
+    console.log(alertComponent.className);
+    expect(alertComponent.hasClass('notification')).to.be.true;
+  });
+
+  it('property assigns class names based on the passed type', () => {
+    const alertComponent = shallow((
+    <Alert type="danger">
+      Danger
+    </Alert>));
+
+    console.log(alertComponent.className);
+    expect(alertComponent.hasClass('is-danger')).to.be.true;
   });
 });
