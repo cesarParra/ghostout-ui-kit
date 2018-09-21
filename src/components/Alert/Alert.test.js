a7df4508f0012cd08c6b002a51600fb6ed3e63ce
Cesar Parra <cesar.parra@gmail.com>
2018-09-21 16:46:45 -0400

    Configuring testing environment for Jest + Enzyme

---
+++ src/components/Alert/Alert.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai'
+
+import Alert from './Alert';
+
+describe('<Alert />', () => {
+  it('renders children when passed in', () => {
+    expect(true).to.equal(true);
+  });
+});
