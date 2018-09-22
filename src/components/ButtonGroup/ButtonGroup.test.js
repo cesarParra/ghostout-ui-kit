4ec1fdb7c41e87267c85d7f90bb2bde295283afa
Cesar Parra <cesar.parra@gmail.com>
2018-09-22 12:57:25 -0400

    Unit tests for the ButtonGroup component

---
+++ src/components/ButtonGroup/ButtonGroup.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai'
+
+import ButtonGroup from './ButtonGroup';
+import Button from '../Button'
+
+describe('<ButtonGroup />', () => {
+  it('renders children when passed in', () => {
+    const buttonGroup = shallow((
+    <Button>
+        <Button />
+    </Button>));
+
+    expect(buttonGroup.contains(<Button />)).to.equal(true);
+  });
+
+  it('has the buttons class', () => {
+    const buttonGroupComponent = shallow((<ButtonGroup />));
+
+    expect(buttonGroupComponent.hasClass('buttons')).to.be.true;
+  });
+
+  it('has the has-addons class', () => {
+    const buttonGroupComponent = shallow((<ButtonGroup />));
+
+    expect(buttonGroupComponent.hasClass('has-addons')).to.be.true;
+  });
+});
