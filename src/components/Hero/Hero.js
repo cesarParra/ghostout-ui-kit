71f00f3a8625b1697caa2917c99bd10076ba0a7c
Cesar Parra <cesar.parra@gmail.com>
2018-10-04 06:50:31 -0400

    Hero component

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Section from '../Section'
+
+export default ({ head, body }) => {
+  return (
+    // TODO: Let's be less optinionated about it being fullheight and the body having the text
+    // centered and receive that data as params. Let's do this when we move to the UI kit
+    <Section className="hero is-fullheight">
+      <div className="hero-head">
+        {head}
+      </div>
+      <div className="hero-body has-text-centered">
+        {body}
+      </div>
+    </Section>
+  )
+}
